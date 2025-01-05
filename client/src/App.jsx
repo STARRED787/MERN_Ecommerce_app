c1439a5bb5093c7ad6f19a84ec2f772a8a11263f
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 01:17:09 +0530

    MERN

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
     <div className="flex flex-col overflow-hidden bg-white">
       <h1 className="text-center">Header Component</h1>
       <Routes>
-        <Route path="/auth" element={<AuthLayout />} />
+        <Route path="/auth" element={<AuthLayout />}></Route>
       </Routes>
     </div>
   );
