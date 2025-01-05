3cda26336b2d2e9ed614cf9ef5566165f2d0cb0a
STARRED787 <malithamalshan7@gmail.com>
2025-01-05 22:47:30 +0530

    MERN

--- server/server.js
+++ server/server.js
@@ -17,4 +17,27 @@ const app = express();
 // Define the port the server will run on
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({}));
+// Enable Cross-Origin Resource Sharing (CORS) for your application
+app.use(
+  cors({
+    // Specify the origin (domain) allowed to make requests to your server
+    // In this case, only requests from "http://localhost:5173/" (your front-end application) are allowed
+    origin: "http://localhost:5173/",
+
+    // Define the HTTP methods that are allowed for cross-origin requests
+    methods: ["GET", "POST", "PUT", "DELETE"],
+
+    // Specify the headers that can be sent with the request from the client
+    allowedHeaders: [
+      "Content-Type", // Specifies the format of the request body (e.g., JSON)
+      "Authorization", // Used for sending authentication tokens (e.g., JWT)
+      "Cache-Control", // Used for caching policies
+      "expires", // Used for expiration metadata
+      "pragma", // Often used in HTTP caching headers
+    ],
+
+    // Enable cookies and other credentials (e.g., authentication tokens) to be sent with cross-origin requests
+    // This is necessary for handling user sessions or authentication
+    credentials: true,
+  })
+);
