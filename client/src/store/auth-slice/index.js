edce6181f9f40c9ae0f8d7a647bc27658a4a2350
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 00:42:25 +0530

    MERN

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -1,20 +1,32 @@
-const { createSlice } = require("@reduxjs/toolkit");
+// Import createSlice from Redux Toolkit to create a slice of the Redux store
+import { createSlice } from "@reduxjs/toolkit";
 
+// Define the initial state of the authentication
 const initialState = {
-  isAuthenticated: false,
-  isLoading: false,
-  user: null,
+  isAuthenticated: false, // Track if the user is authenticated or not
+  isLoading: false, // Track if a request is loading
+  user: null, // Store user information
 };
 
+// Create the authentication slice of the store
 const authSlice = createSlice({
+  // Name of the slice, used to identify the reducer
   name: "auth",
+
+  // Initial state of the slice
   initialState,
+
+  // Reducers define how the state changes in response to actions
   reducers: {
+    // setUser action: This will set the user data in the state
     setUser: (state, action) => {
-      state.user = action.payload;
+      state.user = action.payload; // Update the user field with the payload from the action
     },
   },
 });
 
+// Export the setUser action so it can be dispatched to update the state
 export const { setUser } = authSlice.actions;
+
+// Export the reducer to be included in the store configuration
 export default authSlice.reducer;
