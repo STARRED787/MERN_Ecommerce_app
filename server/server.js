8734f26bb2ce0acb0af1572bafa0e8d5f1354bef
STARRED787 <malithamalshan7@gmail.com>
2025-01-05 22:41:08 +0530

    MERN

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cookieParser = require("cookie-parser"); // Fixed typo: "cokieParser" -> "cookieParser"
-const cors = require("cors");
+const cookieParser = require("cookie-parser"); //parses cookies attached to the client-side request object
+const cors = require("cors"); //Allows your server to handle requests from different origins (domains, ports, or protocols)
 //create database connection
 // create a database connection separate file and import it here
 
@@ -16,3 +16,5 @@ then(() => console.log("Database connected successfully!")).catch((err) =>
 const app = express();
 // Define the port the server will run on
 const PORT = process.env.PORT || 5000;
+
+app.use(cors({}));
