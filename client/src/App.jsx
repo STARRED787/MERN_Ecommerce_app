b5aa6c3719046d217b74dea5af857a14d1a17908
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 01:26:27 +0530

    MERN

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import AuthLayout from "./components/auth/layout";
+import SignIn from "./pages/auth/SignIn";
+import SignUp from "./pages/auth/SignUp";
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <h1 className="text-center">Header Component</h1>
       <Routes>
-        <Route path="/auth" element={<AuthLayout />}></Route>
+        <Route path="/auth" element={<AuthLayout />}>
+          {/* Use relative paths here */}
+          <Route path="signin" element={<SignIn />} />
+          <Route path="signup" element={<SignUp />} />
+        </Route>
       </Routes>
     </div>
   );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,18 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import store from "./store/store";
+// Importing required libraries
+import { createRoot } from "react-dom/client"; // To render the React app
+import "./index.css"; // Importing the main CSS file for styling
+import App from "./App.jsx"; // Importing the main App component
+import { BrowserRouter } from "react-router-dom"; // Importing BrowserRouter for routing
+import { Provider } from "react-redux"; // Importing Provider to integrate Redux with the app
+import store from "./store/store"; // Importing the Redux store
 
+// Rendering the React app inside the root div
 createRoot(document.getElementById("root")).render(
+  // Wrapping the app with BrowserRouter to enable routing
   <BrowserRouter>
+    {/* Wrapping the app with Provider to make Redux store available to the entire app */}
     <Provider store={store}>
-      <App />
+      <App /> {/* The main App component */}
     </Provider>
   </BrowserRouter>
 );
