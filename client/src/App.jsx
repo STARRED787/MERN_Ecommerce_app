268e1d1e59e55dd226ea902f31fb11cc2c809b0f
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 01:14:09 +0530

    MERN

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <h1 className="text-center">Header Component</h1>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />} />
+      </Routes>
     </div>
   );
 }
