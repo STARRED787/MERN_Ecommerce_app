c0505d5c3ba9acb71c7ab1ebe5ec1c456c173cde
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 20:24:49 +0530

    MERN

--- client/src/components/admin/layout.jsx
+++ client/src/components/admin/layout.jsx
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom";
+import AdminSidebar from "./sidebar";
+import AdminHeader from "./header";
 
 function AdminLayout() {
   return (
     <div className="flex min-h-screen w-full">
       {/* Admin Sidebar */}
+      <AdminSidebar />
       <div className="flex flex-1 flex-col">{/* Admin Header */}</div>
       {/* Admin header */}
-      <main>
+      <AdminHeader />
+      <main className="flex-1 flex bg-muted p-4 md:p-6">
         <Outlet />
       </main>
     </div>
