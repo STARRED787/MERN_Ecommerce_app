1c63ff852d574cdd1d3e0a747ace4152ef15f13d
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 21:17:06 +0530

    MERN

--- client/src/components/shopping/layout.jsx
+++ client/src/components/shopping/layout.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
+import ShoppingHeader from "./header";
 
 function ShoppingLayout() {
   return (
     <div className=" flex flex-col bg-white overflow-hidden">
       {/* Header */}
+      <ShoppingHeader />
       <main className="flex-1 flex-col w-full">
         <Outlet />
       </main>
