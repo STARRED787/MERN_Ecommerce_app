6f5904fb58169fd9e02da35893354c419b89d571
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 21:07:30 +0530

    MERN

--- client/src/components/user/layout.jsx
+++ client/src/components/user/layout.jsx
@@ -1,5 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 function UserLayout() {
-  return <div>UserLayout</div>;
+  return (
+    <div className=" flex flex-col bg-white overflow-hidden">
+      {/* Header */}
+      <main className="flex-1 flex-col w-full">
+        <Outlet />
+      </main>
+    </div>
+  );
 }
 
 export default UserLayout;
