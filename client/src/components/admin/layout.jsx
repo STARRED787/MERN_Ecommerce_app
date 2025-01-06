f985fd71255dcf2183905142490b1a6fafc65741
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 20:07:39 +0530

    MERN

--- client/src/components/admin/layout.jsx
+++ client/src/components/admin/layout.jsx
@@ -1,8 +1,14 @@
+import { Outlet } from "react-router-dom";
+
 function AdminLayout() {
   return (
     <div className="flex min-h-screen w-full">
       {/* Admin Sidebar */}
       <div className="flex flex-1 flex-col">{/* Admin Header */}</div>
+      {/* Admin header */}
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
