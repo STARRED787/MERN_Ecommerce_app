f3d94d0e58c06da3a8e8f10a4d17a02d9a5c8a9b
STARRED787 <malithamalshan7@gmail.com>
2025-01-06 22:43:55 +0530

    MERN

--- client/src/components/common/check.jsx
+++ client/src/components/common/check.jsx
@@ -1,17 +1,17 @@
-import { useLocation } from "react-router-dom"
+import { Navigate, useLocation } from "react-router-dom";
 
 function CheckAuth(isAuthenticated, user, children) {
-    const location = useLocation
+  const location = useLocation;
 
-    if (!isAuthenticated && 
-        !(location.pathname.includes('/signin'))
-        || !(location.pathname.includes('/sigup'))
-    ) {
-        
-    }
-  return (
- 
-  )
+  if (
+    !isAuthenticated &&
+    !(
+      location.pathname.includes("/signin") ||
+      location.pathname.includes("/sigup")
+    )
+  ) {
+    return <Navigate to="/signin" />;
+  }
 }
 
-export default CheckAuth
+export default CheckAuth;
