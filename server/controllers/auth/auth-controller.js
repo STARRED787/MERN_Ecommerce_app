2b3f6e8f926956ee63657c7a67d55f6a5974aaf0
STARRED787 <malithamalshan7@gmail.com>
2025-01-08 21:25:26 +0530

    MERN

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -1,1 +1,18 @@
-//regiterc controller
+//register controller
+const register = async (req, res) => {
+  const { username, email, password } = req.body;
+  try {
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      sucess: false,
+      message: "some error occured",
+    });
+  }
+};
+
+//login controller
+
+//logout controller
+
+//auth middleware
