ae000426091648ce2a378c04be9f5ade2c65ddc3
STARRED787 <malithamalshan7@gmail.com>
2025-01-08 21:32:36 +0530

    MERN

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const User = require("../../models/user");
 
 //register controller
 const register = async (req, res) => {
