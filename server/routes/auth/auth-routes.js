09eb0bfee64bdbdc4b1e0b8d6475d2173ad6a2cc
STARRED787 <malithamalshan7@gmail.com>
2025-01-10 16:00:16 +0530

    MERN

--- server/routes/auth/auth-routes.js
+++ server/routes/auth/auth-routes.js
@@ -1,10 +1,17 @@
-const express = require("express");
-const app = express();
-const authRoutes = require("./routes/auth/auth-router"); // Importing the router
+const express = require("express"); // Importing express library
+const {
+  userRegister, // Importing the user registration controller
+  Userlogin, // Importing the user login controller
+} = require("../../controllers/auth/auth-controller"); // Importing controllers from the 'auth-controller' file
 
-app.use(express.json()); // Middleware to parse JSON requests
+const router = express.Router(); // Creating an instance of express.Router() to define routes
 
-// Mount the authentication routes at '/auth'
-app.use("/auth", authRoutes);
+// Define the route for user registration (sign-up)
+router.post("/signup", userRegister);
+// When a POST request is made to the /signup endpoint, the 'userRegister' controller will handle it
 
-// Add other routes and middleware as needed
+// Define the route for user login (sign-in)
+router.post("/signin", Userlogin);
+// When a POST request is made to the /signin endpoint, the 'Userlogin' controller will handle it
+
+module.exports = router; // Export the router to use in other parts of the application
