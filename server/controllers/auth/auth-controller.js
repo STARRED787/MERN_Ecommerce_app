342df10b1262034c96adf5b07c5330b23188bffa
STARRED787 <malithamalshan7@gmail.com>
2025-01-10 15:33:14 +0530

    MERN

--- server/controllers/auth/auth-controller.js
+++ server/controllers/auth/auth-controller.js
@@ -43,6 +43,46 @@ const userRegister = async (req, res) => {
 };
 
 //login controller
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const checkUser = await User.findOne({ email });
+    if (!checkUser) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+    const checkPassword = await bcrypt.compare(password, checkUser.password);
+    if (!checkPassword) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+  } catch (e) {
+    // In your register controller
+    console.log(e);
+    if (e.code === 11000) {
+      // MongoDB Duplicate Key Error
+      if (e.message.includes("username")) {
+        return res.status(400).json({
+          success: false,
+          message: "Username is already taken.",
+        });
+      } else if (e.message.includes("email")) {
+        return res.status(400).json({
+          success: false,
+          message: "Email is already registered.",
+        });
+      }
+    }
+    res.status(500).json({
+      success: false,
+      message: "Some error occurred",
+    });
+  }
+};
 
 //logout controller
 
