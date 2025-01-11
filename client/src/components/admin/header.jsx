0faee3553a6a9af52d733899776d1a9667dabbcc
STARRED787 <malithamalshan7@gmail.com>
2025-01-11 20:55:43 +0530

    MERN

--- client/src/components/admin/header.jsx
+++ client/src/components/admin/header.jsx
@@ -1,5 +1,11 @@
+import { Button } from "../ui/button";
+
 function AdminHeader() {
-  return <div>AdminHeader</div>;
+  return (
+    <header className=" flex items-center justify-between px-4 py-3 bg-background border border-b">
+      <Button></Button>
+    </header>
+  );
 }
 
 export default AdminHeader;
