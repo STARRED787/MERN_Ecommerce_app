8407e8507c7aa3a05e79b32821eda8f1a2aa824a
STARRED787 <malithamalshan7@gmail.com>
2025-01-11 21:15:28 +0530

    MERN

--- client/src/components/admin/sidebar.jsx
+++ client/src/components/admin/sidebar.jsx
@@ -1,5 +1,13 @@
+import { Fragment } from "react";
+
 function AdminSidebar() {
-  return <div>AdminSidebar</div>;
+  return (
+    <Fragment>
+      <aside className="hidden flex-col w-64 border-r bg-background p-6 lg:flex">
+        <div className="flex items-center gap-2"></div>
+      </aside>
+    </Fragment>
+  );
 }
 
 export default AdminSidebar;
