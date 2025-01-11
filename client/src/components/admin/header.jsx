8c3707de08a37f31d249207698528b046179eb06
STARRED787 <malithamalshan7@gmail.com>
2025-01-11 21:00:58 +0530

    MERN

--- client/src/components/admin/header.jsx
+++ client/src/components/admin/header.jsx
@@ -1,9 +1,13 @@
+import { AlignJustify } from "lucide-react";
 import { Button } from "../ui/button";
 
 function AdminHeader() {
   return (
     <header className=" flex items-center justify-between px-4 py-3 bg-background border border-b">
-      <Button></Button>
+      <Button>
+        <AlignJustify />
+        <span>Toggle</span>
+      </Button>
     </header>
   );
 }
