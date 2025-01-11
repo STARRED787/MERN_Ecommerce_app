34e3425f221df55adb76dc1dfb6da9d2955d084e
STARRED787 <malithamalshan7@gmail.com>
2025-01-11 23:14:47 +0530

    MERN

--- client/src/components/admin/layout.jsx
+++ client/src/components/admin/layout.jsx
@@ -4,18 +4,17 @@ import AdminHeader from "./header";
 
 function AdminLayout() {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Admin Header */}
-      <AdminHeader />
+    <div className="flex min-h-screen w-full">
+      {/* Sidebar */}
+      <AdminSidebar />
 
       {/* Sidebar and Main Content */}
-      <div className="flex flex-1">
-        {/* Sidebar */}
-        <AdminSidebar />
+      <div className="flex flex-1 flex-col">
+        {/* Admin Header */}
+        <AdminHeader />
 
         {/* Main Content */}
         <main className="flex-1 bg-muted p-4 md:p-6">
-          <h1>Content</h1>
           <Outlet />
         </main>
       </div>
