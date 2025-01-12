ed056661273a661a2382261d656d182225579305
STARRED787 <malithamalshan7@gmail.com>
2025-01-12 18:13:56 +0530

    MERN

--- client/src/pages/admin/products.jsx
+++ client/src/pages/admin/products.jsx
@@ -1,14 +1,26 @@
 import { Button } from "@/components/ui/button";
-import { Fragment } from "react";
+import { Sheet, SheetContent } from "@/components/ui/sheet";
+import { Fragment, useState } from "react";
 
 function AdminProducts() {
+  const [openCreateProductDialog, setOpenCreateProductDialog] = useState(false);
   return (
     <Fragment>
       <div className="mb-5 w-full flex justify-end">
-        <Button className="bg-orange-500">Add New Product</Button>
+        <Button
+          onClick={() => setOpenCreateProductDialog(true)}
+          className="bg-orange-500 hover:bg-orange-400"
+        >
+          Add New Product
+        </Button>
       </div>
       <div className="grid gap-4 md:grid-cols3 lg:grid-cols-4">
-        title, description, category, brand, price, sale price, total
+        <Sheet
+          open={openCreateProductDialog}
+          onOpenChange={() => setOpenCreateProductDialog(false)}
+        >
+          <SheetContent side="right" className="overflow-auto"></SheetContent>
+        </Sheet>
       </div>
     </Fragment>
   );
