88848ab793181e7d24fbd43b3141e7c999605717
STARRED787 <malithamalshan7@gmail.com>
2025-01-12 20:00:37 +0530

    MERN

--- client/src/components/admin/imageupload.jsx
+++ client/src/components/admin/imageupload.jsx
@@ -1,12 +1,32 @@
-function adminProductImageUpload() {
+import { Label } from "@radix-ui/react-label";
+import { useRef } from "react";
+import { Input } from "../ui/input";
+
+function AdminProductImageUpload({
+  imageFile,
+  setImageFile,
+  uplodedImgeUrl,
+  setUplodedImgeUrl,
+}) {
+  const inputRef = useRef(null);
+
+  function handleImageFileChange(event) {
+    console.log(event.target.files);
+  }
   return (
     <div className="w-full max-w-md mx-auto">
-      <label className="text-lg font-semibold mb-2 block">Upload</label>
+      <Label className="text-lg font-semibold mb-2 block">Upload</Label>
       <div>
-        <input type="file" id="imageUpload" className="hidden" />
+        <Input
+          type="file"
+          id="imageUpload"
+          ref={inputRef}
+          onChange={handleImageFileChange}
+          className=""
+        />
       </div>
     </div>
   );
 }
 
-export default adminProductImageUpload;
+export default AdminProductImageUpload;
