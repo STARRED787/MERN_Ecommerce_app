6de789c087fb96e6ee805715160a28f2211d254e
STARRED787 <malithamalshan7@gmail.com>
2025-01-12 20:09:54 +0530

    MERN

--- client/src/components/admin/imageupload.jsx
+++ client/src/components/admin/imageupload.jsx
@@ -1,6 +1,8 @@
 import { Label } from "@radix-ui/react-label";
 import { useRef } from "react";
 import { Input } from "../ui/input";
+import PropTypes from "prop-types";
+import { UploadCloudIcon } from "lucide-react";
 
 function AdminProductImageUpload({
   imageFile,
@@ -12,7 +14,13 @@ function AdminProductImageUpload({
 
   function handleImageFileChange(event) {
     console.log(event.target.files);
+    const selectedFile = event.target.files?.[0];
+    if (selectedFile) {
+      setImageFile(selectedFile);
+      setUplodedImgeUrl(URL.createObjectURL(selectedFile));
+    }
   }
+
   return (
     <div className="w-full max-w-md mx-auto">
       <Label className="text-lg font-semibold mb-2 block">Upload</Label>
@@ -24,9 +32,25 @@ function AdminProductImageUpload({
           onChange={handleImageFileChange}
           className=""
         />
+        {!imageFile ? (
+          <Label
+            htmlFor="image-upload"
+            className=" flex flex-col items-center justify-center h-32 cursor-pointer"
+          >
+            <UploadCloudIcon size={32} text-muted />
+          </Label>
+        ) : (
+          <div></div>
+        )}
       </div>
     </div>
   );
 }
+AdminProductImageUpload.propTypes = {
+  imageFile: PropTypes.object,
+  setImageFile: PropTypes.func.isRequired,
+  uplodedImgeUrl: PropTypes.string,
+  setUplodedImgeUrl: PropTypes.func.isRequired,
+};
 
 export default AdminProductImageUpload;
