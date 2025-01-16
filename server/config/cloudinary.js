dafc7be5472e21cc9d4345e1137ad36922fa8ca1
STARRED787 <malithamalshan7@gmail.com>
2025-01-16 23:51:18 +0530

    MERN

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -6,3 +6,13 @@ cloudinary.config({
   api_key: "774861489475243",
   api_secret: "uHLqfc_e_mCgB5O_E91tiRBCCuU",
 });
+
+const storage = new multer.memoryStorage();
+
+async function handleImageUpload(file) {
+  const result = await cloudinary.uploader.upload(file, {
+    resource_type: "auto",
+  });
+
+  return result;
+}
