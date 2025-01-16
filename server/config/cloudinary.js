812caaaeace813dcd3b47f7bb031524603522609
STARRED787 <malithamalshan7@gmail.com>
2025-01-16 23:49:01 +0530

    MERN

--- server/config/cloudinary.js
+++ server/config/cloudinary.js
@@ -1,1 +1,8 @@
 const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+
+cloudinary.config({
+  clud_name: "dyphvszup",
+  api_key: "774861489475243",
+  api_secret: "uHLqfc_e_mCgB5O_E91tiRBCCuU",
+});
