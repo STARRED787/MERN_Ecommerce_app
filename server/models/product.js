b0eb204a28f1df8c2931cbd1a65031f097594dad
STARRED787 <malithamalshan7@gmail.com>
2025-01-17 20:17:06 +0530

    MERN

--- server/models/product.js
+++ server/models/product.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  image: String,
+  title: String,
+  description: String,
+  category: String,
+  brand: String,
+  price: Number,
+  salePrice: Number,
+  totatlStock: Number,
+});
