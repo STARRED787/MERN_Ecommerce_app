727ee42bb9d3b97e435ce9a812561b3d15f077d6
STARRED787 <malithamalshan7@gmail.com>
2025-01-20 13:35:30 +0530

    MERN

--- client/src/components/shopping/header.jsx
+++ client/src/components/shopping/header.jsx
@@ -1,5 +1,18 @@
+import { HousePlug } from "lucide-react";
+import { Link } from "react-router-dom";
+
 function ShoppingHeader() {
-  return <div>ShoppingHeader</div>;
+  return (
+    <header className="border-bottom sticky top-0 z-40 w-full bg-slate-900">
+      <div className="flex h-16 items-center justify-between px-4 sm:px-8 lg:px-16 xl:px-24 text-white">
+        <Link className="flex items-center gap-2" to="/shop/home">
+          {" "}
+          <HousePlug className="h6 w-6" />
+          <span className="font-bold">Ecommerce </span>
+        </Link>
+      </div>
+    </header>
+  );
 }
 
 export default ShoppingHeader;
